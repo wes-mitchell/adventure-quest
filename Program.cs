8096460efee69a8b249de2482034fa3deffe60fa
wes-mitchell <wmdrums@gmail.com>
2022-05-27 13:16:52 -0500

    completes phase seven

--- Program.cs
+++ Program.cs
@@ -11,11 +11,7 @@ namespace Quest
         {
             Console.Write("Welcome, please provide your name: ");
             string adventurerName = Console.ReadLine();
-            // Create a few challenges for our Adventurer's quest
-            // The "Challenge" Constructor takes three arguments
-            //   the text of the challenge
-            //   a correct answer
-            //   a number of awesome points to gain or lose depending on the success of the challenge
+
             Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
             Challenge theAnswer = new Challenge(
                 "What's the answer to life, the universe and everything?", 42, 25);
@@ -35,14 +31,27 @@ namespace Quest
                 4, 20
             );
 
-            // "Awesomeness" is like our Adventurer's current "score"
-            // A higher Awesomeness is better
+            Challenge bestDrummer = new Challenge(
+                @"Who's the best drummer?
+                1) John Bonham
+                2) Benny Greb
+                3) Jojo Mayer
+                4) Mark Guiliana
+                ",
+                1, 25
+            );
 
-            // Here we set some reasonable min and max values.
-            //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
-            //  If an Adventurer has an Awesomeness less than the min, they are terrible
+            Challenge bestDrink = new Challenge(
+                @"What's a superior drink?
+                1) Coffee
+                2) Tea
+                ",
+                1, 15
+            );
+
             int minAwesomeness = 0;
             int maxAwesomeness = 100;
+
             List<string> colors = new List<string>
             {
                 "red",
@@ -52,86 +61,109 @@ namespace Quest
                 "purple",
                 "cucumber green"
             };
+
             Robe rando = new Robe
             {
                 Colors = colors, 
                 Length = 45
             };
+
             Hat hatObj = new Hat
             {
                 ShininessLevel = 9
             };
+
             Prize turtlesComic = new Prize("Ninja Turtles Comic book issue one, 1st edition mate!");
-            // Make a new "Adventurer" object using the "Adventurer" class
 
             Adventurer theAdventurer = new Adventurer(adventurerName, rando, hatObj);
+
             Console.WriteLine(theAdventurer.GetDescription());
             
-
-            // A list of challenges for the Adventurer to complete
-            // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
             List<Challenge> challenges = new List<Challenge>()
             {
                 twoPlusTwo,
                 theAnswer,
                 whatSecond,
                 guessRandom,
-                favoriteBeatle
+                favoriteBeatle,
+                bestDrummer,
+                bestDrink
             };
 
             // Loop through all the challenges and subject the Adventurer to them
+            int index = 5;
+
             foreach (Challenge challenge in challenges)
             {
-                challenge.RunChallenge(theAdventurer);
+                while(index != 0)
+                {
+                int randomIndex = new Random().Next(challenges.Count); 
+                challenges[randomIndex].RunChallenge(theAdventurer);
+                index--;
+                }       
             }
 
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
+
             if (theAdventurer.Awesomeness >= maxAwesomeness)
             {
                 Console.WriteLine("YOU DID IT! You are truly awesome!");
                 turtlesComic.ShowPrize(theAdventurer);
+                // Console.Write("Would you like to play again? yes/no: ");
+                // string answer = Console.ReadLine();
             }
             else if (theAdventurer.Awesomeness <= minAwesomeness)
             {
                 Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
                 turtlesComic.ShowPrize(theAdventurer);
+                // Console.Write("Would you like to play again? yes/no: ");
+                // string answer = Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
                 turtlesComic.ShowPrize(theAdventurer);
+                // Console.Write("Would you like to play again? yes/no: ");
+                // string answer = Console.ReadLine();
             }
-
+                index = 5;
                 Console.Write("Would you like to play again? yes/no: ");
                 string answer = Console.ReadLine();
-
-            while(answer.ToUpper() != "NO")
+                
+            while(answer.ToUpper() != "NO" && index != 0) {
             {
                 foreach (Challenge challenge in challenges)
             {
-                challenge.RunChallenge(theAdventurer);
+
+                while(index != 0)
+                {
+                int randomIndex = new Random().Next(challenges.Count); 
+                challenges[randomIndex].RunChallenge(theAdventurer);
+                index--;
+                }     
             }
 
             if (theAdventurer.Awesomeness >= maxAwesomeness)
             {
                 Console.WriteLine("YOU DID IT! You are truly awesome!");
                 turtlesComic.ShowPrize(theAdventurer);
+                Console.Write("Would you like to play again? yes/no: ");
+                answer = Console.ReadLine();
             }
             else if (theAdventurer.Awesomeness <= minAwesomeness)
             {
                 Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
                 turtlesComic.ShowPrize(theAdventurer);
+                Console.Write("Would you like to play again? yes/no: ");
+                answer = Console.ReadLine();
             }
             else
             {
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
                 turtlesComic.ShowPrize(theAdventurer);
-            }
                 Console.Write("Would you like to play again? yes/no: ");
                 answer = Console.ReadLine();
             }
+
             }
-        }
-    }
+            }}}}
     
